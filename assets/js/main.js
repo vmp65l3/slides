a8fc213c9c592d217f3b98e6d775acef3ab25977
O O <oo@zhaochaojundeMacBook-Air.local>
2022-07-27 12:10:45 +0800

    hs

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,5 @@
 /*
-<<<<<<< HEAD
 	Hyperspace by HTML5 UP
-=======
-	Paradigm Shift by HTML5 UP
->>>>>>> d1d2dbe82d19253f2def47e9fcee3844cde23dac
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
@@ -11,7 +7,6 @@
 (function($) {
 
 	var	$window = $(window),
-<<<<<<< HEAD
 		$body = $('body'),
 		$sidebar = $('#sidebar');
 
@@ -22,19 +17,6 @@
 			medium:   [ '737px',   '980px'  ],
 			small:    [ '481px',   '736px'  ],
 			xsmall:   [ null,      '480px'  ]
-=======
-		$body = $('body');
-
-	// Breakpoints.
-		breakpoints({
-			default:   ['1681px',   null       ],
-			xlarge:    ['1281px',   '1680px'   ],
-			large:     ['981px',    '1280px'   ],
-			medium:    ['737px',    '980px'    ],
-			small:     ['481px',    '736px'    ],
-			xsmall:    ['361px',    '480px'    ],
-			xxsmall:   [null,       '360px'    ]
->>>>>>> d1d2dbe82d19253f2def47e9fcee3844cde23dac
 		});
 
 	// Hack: Enable IE flexbox workarounds.
@@ -48,7 +30,6 @@
 			}, 100);
 		});
 
-<<<<<<< HEAD
 	// Forms.
 
 		// Hack: Activate non-input submits.
@@ -71,219 +52,82 @@
 			$sidebar_a
 				.addClass('scrolly')
 				.on('click', function() {
-=======
-	// Hack: Enable IE workarounds.
-		if (browser.name == 'ie')
-			$body.addClass('is-ie');
 
-	// Mobile?
-		if (browser.mobile)
-			$body.addClass('is-mobile');
+					var $this = $(this);
 
-	// Scrolly.
-		$('.scrolly')
-			.scrolly({
-				offset: 100
-			});
+					// External link? Bail.
+						if ($this.attr('href').charAt(0) != '#')
+							return;
 
-	// Polyfill: Object fit.
-		if (!browser.canUse('object-fit')) {
-
-			$('.image[data-position]').each(function() {
-
-				var $this = $(this),
-					$img = $this.children('img');
-
-				// Apply img as background.
-					$this
-						.css('background-image', 'url("' + $img.attr('src') + '")')
-						.css('background-position', $this.data('position'))
-						.css('background-size', 'cover')
-						.css('background-repeat', 'no-repeat');
-
-				// Hide img.
-					$img
-						.css('opacity', '0');
-
-			});
-
-			$('.gallery > a').each(function() {
-
-				var $this = $(this),
-					$img = $this.children('img');
->>>>>>> d1d2dbe82d19253f2def47e9fcee3844cde23dac
-
-				// Apply img as background.
-					$this
-						.css('background-image', 'url("' + $img.attr('src') + '")')
-						.css('background-position', 'center')
-						.css('background-size', 'cover')
-						.css('background-repeat', 'no-repeat');
-
-				// Hide img.
-					$img
-						.css('opacity', '0');
-
-<<<<<<< HEAD
 					// Deactivate all links.
 						$sidebar_a.removeClass('active');
-=======
-			});
->>>>>>> d1d2dbe82d19253f2def47e9fcee3844cde23dac
 
-		}
+					// Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
+						$this
+							.addClass('active')
+							.addClass('active-locked');
 
-	// Gallery.
-		$('.gallery')
-			.on('click', 'a', function(event) {
+				})
+				.each(function() {
 
-				var $a = $(this),
-					$gallery = $a.parents('.gallery'),
-					$modal = $gallery.children('.modal'),
-					$modalImg = $modal.find('img'),
-					href = $a.attr('href');
+					var	$this = $(this),
+						id = $this.attr('href'),
+						$section = $(id);
 
-				// Not an image? Bail.
-					if (!href.match(/\.(jpg|gif|png|mp4)$/))
-						return;
+					// No section for this link? Bail.
+						if ($section.length < 1)
+							return;
 
-<<<<<<< HEAD
 					// Scrollex.
 						$section.scrollex({
 							mode: 'middle',
 							top: '-20vh',
 							bottom: '-20vh',
 							initialize: function() {
-=======
-				// Prevent default.
-					event.preventDefault();
-					event.stopPropagation();
->>>>>>> d1d2dbe82d19253f2def47e9fcee3844cde23dac
 
-				// Locked? Bail.
-					if ($modal[0]._locked)
-						return;
+								// Deactivate section.
+									$section.addClass('inactive');
 
-				// Lock.
-					$modal[0]._locked = true;
+							},
+							enter: function() {
 
-				// Set src.
-					$modalImg.attr('src', href);
+								// Activate section.
+									$section.removeClass('inactive');
 
-<<<<<<< HEAD
 								// No locked links? Deactivate all links and activate this section's one.
 									if ($sidebar_a.filter('.active-locked').length == 0) {
 
 										$sidebar_a.removeClass('active');
 										$this.addClass('active');
-=======
-				// Set visible.
-					$modal.addClass('visible');
 
-				// Focus.
-					$modal.focus();
->>>>>>> d1d2dbe82d19253f2def47e9fcee3844cde23dac
+									}
 
-				// Delay.
-					setTimeout(function() {
+								// Otherwise, if this section's link is the one that's locked, unlock it.
+									else if ($this.hasClass('active-locked'))
+										$this.removeClass('active-locked');
 
-						// Unlock.
-							$modal[0]._locked = false;
+							}
+						});
 
-					}, 600);
+				});
 
-			})
-			.on('click', '.modal', function(event) {
-
-<<<<<<< HEAD
 		}
-=======
-				var $modal = $(this),
-					$modalImg = $modal.find('img');
 
-				// Locked? Bail.
-					if ($modal[0]._locked)
-						return;
->>>>>>> d1d2dbe82d19253f2def47e9fcee3844cde23dac
+	// Scrolly.
+		$('.scrolly').scrolly({
+			speed: 1000,
+			offset: function() {
 
-				// Already hidden? Bail.
-					if (!$modal.hasClass('visible'))
-						return;
-
-<<<<<<< HEAD
 				// If <=large, >small, and sidebar is present, use its height as the offset.
 					if (breakpoints.active('<=large')
 					&&	!breakpoints.active('<=small')
 					&&	$sidebar.length > 0)
 						return $sidebar.height();
-=======
-				// Stop propagation.
-					event.stopPropagation();
->>>>>>> d1d2dbe82d19253f2def47e9fcee3844cde23dac
 
-				// Lock.
-					$modal[0]._locked = true;
+				return 0;
 
-				// Clear visible, loaded.
-					$modal
-						.removeClass('loaded')
-
-				// Delay.
-					setTimeout(function() {
-
-						$modal
-							.removeClass('visible')
-
-						setTimeout(function() {
-
-							// Clear src.
-								$modalImg.attr('src', '');
-
-							// Unlock.
-								$modal[0]._locked = false;
-
-							// Focus.
-								$body.focus();
-
-						}, 475);
-
-					}, 125);
-
-			})
-			.on('keypress', '.modal', function(event) {
-
-				var $modal = $(this);
-
-				// Escape? Hide modal.
-					if (event.keyCode == 27)
-						$modal.trigger('click');
-
-			})
-			.on('mouseup mousedown mousemove', '.modal', function(event) {
-
-				// Stop propagation.
-					event.stopPropagation();
-
-			})
-			.prepend('<div class="modal" tabIndex="-1"><div class="inner"><img src="" /></div></div>')
-				.find('img')
-					.on('load', function(event) {
-
-						var $modalImg = $(this),
-							$modal = $modalImg.parents('.modal');
-
-						setTimeout(function() {
-
-							// No longer visible? Bail.
-								if (!$modal.hasClass('visible'))
-									return;
-
-							// Set loaded.
-								$modal.addClass('loaded');
-
-						}, 275);
-
-					});
+			}
+		});
 
 	// Spotlights.
 		$('.spotlights > section')
